a38792c115151256ca3e47e2f08e48f407e61e71
dayeison87 <dayeison87@gmail.com>
2026-06-16 11:32:01 -0500

    Adaptacion para despliegue en Render

--- app.py
+++ app.py
@@ -107,4 +107,5 @@ def eliminar_peritaje(placa):
         return jsonify({"error": f"Error al procesar la eliminación: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
